ce4196343118fd7c765901b11b33835f4f8e5872
Marco Oquendo <marcooquendoc@gmail.com>
2022-11-23 00:59:50 -0500

    Add student class file

---
+++ student.rb
@@ -0,0 +1,12 @@
+require './person.rb'
+
+class Student < Person
+  def initialize(age, name = "Unknown", parent_permission = true, classroom = "None")
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
+end
