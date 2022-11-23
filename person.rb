422338eec43d350f293983cf0d00631785215e13
Marco Oquendo <marcooquendoc@gmail.com>
2022-11-23 03:24:50 -0500

    decorate a class

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require 'nameable'
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
@@ -15,6 +17,10 @@ class Person
   # public methods
   def can_use_services?
     is_of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 
   # private methods
