0e34a2986adb74e3050de5b863ffb39fb5a8f5cf
Marco Oquendo <marcooquendoc@gmail.com>
2022-11-26 01:12:06 -0500

    Update classroom.rb

--- classroom.rb
+++ classroom.rb
@@ -7,7 +7,7 @@ class Classroom
   attr_accessor :label
 
   def add_student(student)
-    @students.push(student)
+    @students << student unless @students.include?(student)
     student.classroom = self
   end
 
