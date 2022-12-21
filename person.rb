4a63e898850fe1f1ebccb242dfc7fed85a34ed33
Marco <marcooquendoc@gmail.com>
2022-12-20 22:46:28 -0500

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,26 +1,25 @@
 class Person
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  attr_reader :id
+  attr_accessor :name, :age, :parent_permission
+  
+  def initialize (
+      age,
+      name = "Unknown",
+      parent_permission = true
+    )
     @id = Random.rand(1..1000)
-    @name = name
     @age = age
+    @name = name
     @parent_permission = parent_permission
   end
 
-  # getters
-  attr_reader :id
-
-  # accessors (getters and setters)
-  attr_accessor :name, :age
-
-  # public methods
   def can_use_services?
     is_of_age? || @parent_permission
   end
 
-  # private methods
   private
-
-  def of_age?
+  
+  def is_of_age?
     @age >= 18
   end
 end
