494fd20bcfe6704b4495fe47d4d3b34c77930e2e
Marco <marcooquendoc@gmail.com>
2022-12-20 23:04:21 -0500

    Fix offenses

--- student.rb
+++ student.rb
@@ -2,13 +2,13 @@ require_relative './person'
 
 class Student < Person
   attr_accessor :classroom
-  
-  def initialize (
-      classroom,
-      age,
-      name = "Unknown",
-      parent_permission = true
-    )
+
+  def initialize(
+    classroom,
+    age,
+    name = 'Unknown',
+    parent_permission: true
+  )
     super(age, name, parent_permission)
     @classroom = classroom
   end
