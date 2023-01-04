46100c5b200cb9b5801e2eca9ae1a27ffd01c5d7
Marco Oquendo <marcooquendoc@gmail.com>
2023-01-04 14:58:57 -0500

    Add person class tests

---
+++ spec/person_spec.rb
@@ -0,0 +1,18 @@
+require_relative './../person'
+require_relative './../decorators/nameable'
+
+describe "Test person object" do
+  person = Person.new(18)
+
+  it "takes parameters and returns a Nameable object" do
+    expect(person).to be_kind_of(Nameable)
+  end
+
+  it "takes parameters and returns a Person object" do
+    expect(person).to be_instance_of(Person)
+  end
+
+  it "person name should be 'Unknown' when not defined" do
+    expect(person.name).to eql 'Unknown'
+  end
+end
