b868aaca494e83fd0ad4514ee62c047886ddf17b
IngDavidTM <david-1011@hotmail.com>
2023-01-03 23:29:32 -0500

    fix empty files

--- data/read_data.rb
+++ data/read_data.rb
@@ -3,16 +3,16 @@ require 'json'
 module ReadData
   def read_people
     json_from_file = File.read('./data/people.json')
-    JSON.parse(json_from_file)
+    File.zero?('./data/people.json') ? [] : JSON.parse(json_from_file)
   end
 
   def read_books
     json_from_file = File.read('./data/books.json')
-    JSON.parse(json_from_file)
+    File.zero?('./data/books.json') ? [] : JSON.parse(json_from_file)
   end
 
   def read_rentals
     json_from_file = File.read('./data/rentals.json')
-    JSON.parse(json_from_file)
+    File.zero?('./data/rentals.json') ? [] : JSON.parse(json_from_file)
   end
 end
