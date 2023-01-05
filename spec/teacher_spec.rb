2535f4bc4f575502be42a412a269d8c973deae79
Marco Oquendo <marcooquendoc@gmail.com>
2023-01-05 01:21:46 -0500

    Fix offenses

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -5,23 +5,23 @@ require_relative './../decorators/nameable'
 describe Teacher do
   context 'Teacher instance behavior' do
     teacher = Teacher.new('Philosophy', 18, 'Test Name')
-    
+
     it 'takes parameters and returns a Person object' do
       expect(teacher).to be_kind_of(Person)
     end
-    
+
     it 'takes parameters and returns a Nameable object' do
       expect(teacher).to be_kind_of(Nameable)
     end
-    
+
     it 'takes parameters and returns a Student object' do
       expect(teacher).to be_instance_of(Teacher)
     end
-    
+
     it "teacher name should be 'Test Name'" do
       expect(teacher.name).to eql 'Test Name'
     end
-    
+
     it "teacher specialization should be 'Philosophy'" do
       expect(teacher.specialization).to eql 'Philosophy'
     end
